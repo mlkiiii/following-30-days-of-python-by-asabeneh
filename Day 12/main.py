3694b5adc51ae98d66e471969f68b34ebff336da
malki mohamed <malkimohamed200506@gmail.com>
2026-03-11 08:01:06 +0100

    fixed the main file

--- Day 12/main.py
+++ Day 12/main.py
@@ -1,5 +1,5 @@
 from mymodule import*
-#print(shuffle_list(['#a3e12f', '#03ed55', '#eb3d2b']))
+print(shuffle_list(['#a3e12f', '#03ed55', '#eb3d2b']))
 user_id_gen_by_user()
 print(rgb_color_gen())
 print(seven_numbers())
